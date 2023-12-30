864eb0b76ce0308cd3bbd936f23e7b79c9a41f89
JosHenryK <119651219+JosHenryK@users.noreply.github.com>
2023-12-29 21:40:06 -0500

    moved contents of main.py to acceleratorModule.py, added accelerator_module.print_proj_stats()

--- acceleratorModule.py
+++ acceleratorModule.py
@@ -7,13 +7,34 @@ class accelerator_module():
         self.delta_x = delta_x
         self.force = pow((num_of_turns * current) , 2) * (12.566 * pow(10 , -7)) * (cross_sectional_area / (2 * pow(gap , 2)))
 
-    def calc_projectile_stats(self, mass , initial_velocity):
+    def calc_proj_final_velocity(self , mass , initial_velocity):
         acceleration = self.force / mass
         final_velocity = pow((initial_velocity + (2 * acceleration * self.delta_x)) , 0.5)
-        time = (final_velocity - initial_velocity) / acceleration
-        print(final_velocity)
-        print(time)
+        return final_velocity
 
-#impulse = force * time
-#delta_velocity = impulse / mass
-#delta_kinetic_energy = 0.5 * mass * pow(delta_velocity , 2)
+    def print_proj_stats(self , mass , initial_velocity):
+        final_velocity = self.calc_proj_final_velocity(mass , initial_velocity)
+        delta_velocity = final_velocity - initial_velocity
+        initial_kinetic_energy = 0.5 * mass * pow(initial_velocity , 2)
+        final_kinetic_energy = 0.5 * mass * pow(final_velocity , 2)
+        delta_kinetic_energy = (0.5 * mass * pow(final_velocity , 2)) - (0.5 * mass * pow(initial_velocity , 2))
+        acceleration = self.force / mass
+        time = (final_velocity - initial_velocity) / acceleration
+        impulse = self.force * time
+        
+        print()
+
+        print("Initial Velocity: " + str(initial_velocity) + " m/s")
+        print("final velocity: " + str(final_velocity) + " m/s")
+        print("delta velocity: " + str(delta_velocity) + " m/s")
+        print()
+
+        print("initial kinetic energy: " + str(initial_kinetic_energy) + " J")
+        print("final kinetic energy: " + str(final_kinetic_energy) + " J")
+        print("delta kinetic energy: " + str(delta_kinetic_energy) + " J")
+        print()
+
+        print("Time: " + str(time) + " s")
+        print("Acceleration: " + str(acceleration) + " m/s^2")
+        print("Impulse: " + str(impulse) + " Ns")
+        print()
--- main.py
+++ main.py
@@ -1,26 +1,4 @@
-num_of_turns = 100
-current = 10
-cross_sectional_area = 0.0000636172512
-gap = 0.001
-force = pow((num_of_turns * current) , 2) * (12.566 * pow(10 , -7)) * (cross_sectional_area / (2 * pow(gap , 2)))
+from acceleratorModule import accelerator_module
 
-mass = 0.008
-delta_x = 0.0127
-
-acceleration = force / mass
-initial_velocity = 0
-final_velocity = pow((initial_velocity + (2 * acceleration * delta_x)) , 0.5)
-time = (final_velocity - initial_velocity) / acceleration
-
-impulse = force * time
-delta_velocity = impulse / mass
-delta_kinetic_energy = 0.5 * mass * pow(delta_velocity , 2)
-
-def accelerator_module(num_of_turns , current , cross_sectional_area , gap , mass , delta_x , initial_velocity):
-    force = pow((num_of_turns * current) , 2) * (12.566 * pow(10 , -7)) * (cross_sectional_area / (2 * pow(gap , 2)))
-    acceleration = force / mass
-    final_velocity = pow((initial_velocity + (2 * acceleration * delta_x)) , 0.5)
-    time = (final_velocity - initial_velocity) / acceleration
-    impulse = force * time
-    delta_velocity = impulse / mass
-    delta_kinetic_energy = 0.5 * mass * pow(delta_velocity , 2)
+module = accelerator_module(100 , 10 , 0.0000636172512 , 0.001 , 0.0127)
+module.print_proj_stats(0.008 , 0)
